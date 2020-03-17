370b1ad393604260933e17fc47733d0fcdd4e9e3
Sohee Lee <sohee6030@gmail.com>
2020-03-17 21:59:53 +0900

    마스크 사자

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import puppeteer from 'puppeteer-core'
+
+(async () => {
+  const AER = 'https://smartstore.naver.com/aer-shop/products/4722827602'
+  const testUrl = 'https://smartstore.naver.com/hyslovely/products/567515619'
+  const browser = await puppeteer.launch({
+    headless: false,
+    executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+  })
+  const page = await browser.newPage()
+  await page.goto(AER)
+
+  const waitBuyButton = async () => {
+    const result = await page.waitForSelector('._buy_button', {
+      timeout: 500
+    })
+      .then(_ => true)
+      .catch(_ => false)
+
+    if (!result) {
+      await page.reload()
+      await waitBuyButton()
+    }
+  }
+
+  await waitBuyButton()
+  await page.click('._buy_button')
+
+})()
