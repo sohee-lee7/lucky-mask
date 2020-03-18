65667071b834cca04517a6daeb7af09e1be852f2
sohee.lee <sohee6030@gmail.com>
2020-03-18 09:37:19 +0900

    로그인 시간을 주자

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,17 @@ import puppeteer from 'puppeteer-core'
   const testUrl = 'https://smartstore.naver.com/hyslovely/products/567515619'
   const browser = await puppeteer.launch({
     headless: false,
+    defaultViewport: null,
     executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
   })
   const page = await browser.newPage()
   await page.goto(AER)
+
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve()
+    }, 20000)
+  })
 
   const waitBuyButton = async () => {
     const result = await page.waitForSelector('._buy_button', {
